22d17e34716fd2d18c64129fe25ddc6a78838097
Xelarion <xelarionz@gmail.com>
2025-04-16 19:48:17 +0800

    refactor(swagger): improve tag determination by converting camelCase to snake_case

--- tools/swagger_autocomment/main.go
+++ tools/swagger_autocomment/main.go
@@ -1191,6 +1191,15 @@ func generateDescription(handlerName string) string {
 
 // determineTagFromHandler determines the Swagger tag from a handler name
 func determineTagFromHandler(handlerName string) string {
-	// Remove Handler suffix and convert to lowercase
-	return strings.ToLower(strings.TrimSuffix(handlerName, "Handler"))
+	// Remove Handler suffix and convert camelCase to lowercase snake case
+	return camelToSnake(strings.TrimSuffix(handlerName, "Handler"))
+}
+
+func camelToSnake(s string) string {
+	var matchFirstCap = regexp.MustCompile("(.)([A-Z][a-z]+)")
+	var matchAllCap = regexp.MustCompile("([a-z0-9])([A-Z])")
+
+	snake := matchFirstCap.ReplaceAllString(s, "${1}_${2}")
+	snake = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
+	return strings.ToLower(snake)
 }
